5103bd51ddf9ad501fc922c76022493a1bf77bc5
Patrick Fulton <patrick@bearded.com>
2013-07-03 16:42:50 -0400

    Convert kit to HAML Remove unnecessary files. Alter app.rb to use HAML

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
-source :rubygems
+source "https://rubygems.org"
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'haml'
+gem 'sass'
 gem 'compass'
 gem 'heroku'
 gem 'breakpoint'
--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/contrib/all'
+require 'haml'
 require 'compass'
 require 'breakpoint'
 
@@ -10,7 +12,7 @@ set :views, "views"
 set :public_folder, 'static'
 
 configure do
-  set :haml, {:format => :html5, :escape_html => true}
+  set :haml, {:format => :html5, :escape_html => false}
   set :scss, {:style => :compact, :debug_info => false}
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
 end
@@ -21,17 +23,17 @@ get '/:version/stylesheets/:name.css' do
 end
 
 get '/' do
-  erb :"v1/index.html"
+  haml :"v1/index.html", :layout => :"v1/layout"
 end
 
 get '/:page_name.html' do
-  erb :"v1/#{params[:page_name]}.html"
+  haml :"v1/#{params[:page_name]}.html", :layout => :"v1/layout"
 end
 
 get '/:version/?' do
-  erb :"#{params[:version]}/index.html"
+  haml :"#{params[:version]}/index.html", :layout => :"#{params[:version]}/layout"
 end
 
 get '/:version/:page_name.html' do
-  erb :"#{params[:version]}/#{params[:page_name]}.html"
+  haml :"#{params[:version]}/#{params[:page_name]}.html", :layout => :"#{params[:version]}/layout"
 end
